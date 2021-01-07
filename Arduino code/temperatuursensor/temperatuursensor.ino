973f5e925e1e6ce0fc679af6e4a436787529140d
kevin <kevin.vervloet@student.kdg.be>
2021-01-07 14:04:05 +0100

    Mappen structuur aangepast

---
+++ Arduino code/temperatuursensor/temperatuursensor.ino
@@ -0,0 +1,48 @@
+//variabelen temperatuursensor
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+// DHT pin instellen
+#define DHTPIN 20
+
+// DHT type instellen
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht = DHT(DHTPIN, DHTTYPE);
+
+//vairabelen millis die ik gebruik voor de temperatuursensor
+const int meetTijd = 30000;
+unsigned long currentTime = 0;
+unsigned long previousTime = 0;
+
+
+void setup() {
+ //setup temperatuursensor
+ Serial.begin(9600);
+ dht.begin();
+
+}
+
+void loop() {
+  // de code voor de temperatuursensor
+  currentTime3 = millis();
+  if (currentTime - previousTime > meetTijd) {
+
+
+    printVochtigheid() ;
+    printTemperatuur() ;
+    previousTime = currentTime;
+  }
+}
+
+void printTemperatuur() {
+  float t = dht.readTemperature();
+  Serial.print(F("% Temperatuur: "));
+  Serial.print(t);
+  Serial.print(F("°C |  "));
+  Serial.print('\n');
+}
+void printVochtigheid() {
+  float h = dht.readHumidity();
+  Serial.print(F("Vochtigheid: "));
+  Serial.print(h);
+}
